16115022d713a16f77756080bb840b5c5cf1d35e
lszme <lszme@10.146.193.228>
2026-03-30 19:58:38 -0300

    init: Início do projeto.

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int P;
+        double A;
+        double imc;
+        P = Integer.parseInt(JOptionPane.showInputDialog("Digite seu peso (KG) : "));
+        A = Double.parseDouble(JOptionPane.showInputDialog("Digite sua altura (M) : "));
+        imc = P /(A * A);
+        JOptionPane.showMessageDialog(null, "O seu imc é: " + imc);
+        
+        
+    }
+}
